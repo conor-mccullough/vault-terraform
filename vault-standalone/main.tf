4c8bbb2488c4cb013d9e41c353990693657cd7a5
Conor McCullough <103977699+conor-mccullough@users.noreply.github.com>
2023-04-23 12:38:38 +1000

    Update main.tf
    Removed region-specific config pamareters to make change requirements more obvious

--- vault-standalone/main.tf
+++ vault-standalone/main.tf
@@ -1,27 +1,29 @@
 # Configure AWS provider
 provider "aws" {
-  region = "ap-northeast-2"
+  region = "CHANGEME"
 }
 
 # Create an EC2 instance for each Vault node, define its security groups, and provide it with a public IP
 resource "aws_instance" "conor-tf-vault" {
   count          = 1
-  ami            = "ami-0ab04b3ccbadfae1f"
-  instance_type  = "t3.micro"
+  ami            = "CHANGEME"
+  instance_type  = "CHANGEME"
   key_name       = "CHANGEME"
   associate_public_ip_address = true
   subnet_id = "${aws_subnet.conor-subnet.id}"
+  # Give your security group a distinct name so that it is easily identifiable.
   # security_groups is an array, so it needs to be in square brackets:
   security_groups = ["${aws_security_group.conor-vault-sg.id}"]
   tags = {
-    Name = "conor-tf-test"
+    Name = "CHANGEME"
   }
 
-  # Provisioner for the SSH connection to the server for subsequent Vault file provisioning
+  # Provisioner for the SSH connection to the server for subsequent Vault file transfers
+  # Verify the AMI type used above, and select the appropriate default username
   connection {
     host = "${self.public_ip}"
     type = "ssh"
-    user = "ubuntu"
+    user = "CHANGEME"
     private_key = "${file("/path/to/key/CHANGEME")}"
     timeout = "2m"
   }
